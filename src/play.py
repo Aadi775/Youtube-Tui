fd612ea909f5d1fa08a8771b416a6168ac2cbd42
Aadi775 <77189096+Aadi775@users.noreply.github.com>
2022-03-14 21:48:36 +0530

    Add files via upload

---
+++ src/play.py
@@ -0,0 +1,22 @@
+import subprocess
+import shlex
+import sys
+import getopt
+
+argumentList = sys.argv[1:]
+
+ptions="hl:"
+
+long_options = ["help","link"]
+
+try:
+    arguments, values = getopt.getopt(argumentList,options,long_options)
+
+    for currentArgument,currentValue in arguments:
+        if currentArgument in ("-h","--help"):
+            print("play.py -l <your yt video link>")
+        elif currentArgument in("-l","--link"):
+            link = currentValue
+            subprocess.call(shlex.split(f'/home/aadi/youtube-tui/src/play.sh {link}'))
+except getopt.error as err:
+    print(str(err))
